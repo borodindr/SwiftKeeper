2771946f280d9c1a64e60ad180e49c127b0f0477
Dmitry Borodin <borodindr@gmail.com>
2023-10-04 22:07:26 -0500

    Added example app

---
+++ Examples/SwiftKeeperExample/SwiftKeeperExample/SwiftKeeperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftKeeperExampleApp.swift
+//  SwiftKeeperExample
+//
+//  Created by Dmitry Borodin on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftKeeperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
