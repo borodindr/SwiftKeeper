02ea017b17d70b14a7c3d30ce585b15db13bd0e4
borodindr <46873400+borodindr@users.noreply.github.com>
2023-10-04 22:25:57 -0500

    Fix supported platforms (#3)
    * Updated platforms
    
    * Reordered platforms

--- Package.swift
+++ Package.swift
@@ -8,19 +8,16 @@ let package = Package(
     platforms: [
         .iOS(.v13),
         .macCatalyst(.v13),
-        .watchOS(.v4),
         .tvOS(.v13),
-        .macOS(.v10_13)
+        .watchOS(.v6),
+        .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftKeeper",
             targets: ["SwiftKeeper"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftKeeper"),
         .testTarget(
